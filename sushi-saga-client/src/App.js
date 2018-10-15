673a589e8697214ab4b2100eea90f28bce59f33d
kristinatong <kristina.tong@gmail.com>
2018-10-15 11:04:56 -0400

    didn't finish

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,40 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    sushis: [],
+    eaten: [],
+    nextFourSushis: [],
+    sushiIndex:4,
+    cash: 100
+  }
+
+  componentDidMount(){
+    fetch(API).then(res => res.json())
+      .then(data => this.setState({
+        sushis:data,
+        nextFourSushis:data.slice(0,4)
+      }))
+  }
+
+  handleSushiClick = (event) => {
+    debugger
+  }
+
+  handleMoreClick = () =>{
+    this.setState({
+      nextFourSushis:this.state.sushis.slice(this.state.sushiIndex,this.state.sushiIndex+4),
+      sushiIndex:this.state.sushiIndex+4
+    })
+  }
+
 
   render() {
+    console.log(this.state)
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer sushis={this.state.nextFourSushis} handleSushiClick={this.handleSushiClick} handleMoreClick={this.handleMoreClick}/>
+        <Table cash={this.state.cash} selected={this.state.selected}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,15 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          props.sushis.map(sushi => <Sushi handleSushiClick={props.handleSushiClick} sushi={sushi}/>)
         }
-        <MoreButton />
+        <MoreButton handleMoreClick={props.handleMoreClick}/>
       </div>
     </Fragment>
   )
