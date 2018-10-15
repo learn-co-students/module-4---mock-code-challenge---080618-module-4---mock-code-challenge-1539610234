debe84eb858ec6ab0dbc1c71feadae06e2f743e6
github username <github email address>
2018-10-15 13:01:01 -0400

    completed

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,66 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sushi : [],
+      selectedSushi: [],
+      emptyPlates: [],
+      wallet: 100
+    }
+  }
+
+  componentDidMount() {
+    fetch(API)
+    .then(r => r.json())
+    .then(data => {
+      let randomFour = []
+      for (let i = 0; i < 4; i++) {
+        randomFour.push(data[Math.floor(Math.random()*100)])
+      }
+
+
+      this.setState({
+        sushi: data,
+        selectedSushi: randomFour
+      })
+    })
+
+  }
+  handlePurchase = (sushiData) => {
+    this.setState({
+      emptyPlates: [
+        ...this.state.emptyPlates,
+        sushiData.data
+      ],
+      wallet: this.state.wallet - sushiData.data.price
+    },() => console.log(this.state.emptyPlates))
+  }
+
+  randomize = (event) => {
+    let randomFour = []
+    let usedKeys = []
+    while (randomFour.length < 5) {
+      const randKey = Math.floor(Math.random()*100)
+      if (!usedKeys.includes(randKey)) {
+        randomFour.push(this.state.sushi[randKey])
+        usedKeys.push(randKey)
+      }
+
+    }
+    this.setState({
+      selectedSushi: randomFour.slice(0,4)
+    })
+    // I added the slice random 4 as a safeguard against multiple sushi rendering
+    // on the screen
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer selectedSushi={this.state.selectedSushi} randomize={this.randomize} handlePurchase={this.handlePurchase} wallet={this.state.wallet} />
+        <Table wallet={this.state.wallet} emptyPlates={this.state.emptyPlates} />
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,39 @@
 import React, { Fragment } from 'react'
 
-const Sushi = (props) => {
-  return (
-    <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
-        }
+class Sushi extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isClicked: false
+    }
+  }
+  handleClick = (event) => {
+    if (this.props.data.price <= this.props.wallet) {
+      if (!this.state.isClicked) {
+        this.props.handlePurchase(this.props)
+      }
+      this.setState({
+        isClicked: true
+      })
+    }
+
+
+  }
+  render() {
+    return (
+      <div className="sushi">
+        <div className="plate"
+          onClick={this.handleClick}>
+          {this.state.isClicked ? null : <img src={this.props.data.img_url} width="100%" /> }
+
+        </div>
+        <h4 className="sushi-details">
+          {this.props.data.name} - ${this.props.data.price}
+        </h4>
       </div>
-      <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
-      </h4>
-    </div>
-  )
+    )
+  }
 }
+
 
 export default Sushi
